680d909b88225410c611cb0d152e246e4372d964
connorshea15 <connorshea15@gmail.com>
2020-06-09 18:41:06 -0700

    Got a simple for loop working!

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Play robot has defeated all enemy robots
+//  * Fight all enemy robots
+//  * Defeat each enemy robot
+// "LOSE" - Play robot's health is zero or less
+
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -7,11 +13,12 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -70,4 +77,6 @@ var fight = function() {
     }
   };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
